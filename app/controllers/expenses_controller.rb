3e19498d24a3860f76060c61ae68ce53fe4f77cd
stivendiaz <stivendiazagudelo@gmail.com>
2018-10-01 22:28:41 -0500

    [Add] ajax for asynchronic filtration but it only filters by one field

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,31 @@
 class ExpensesController < ApplicationController
   def index
-    # current_date = Date.today
-    # current_month = current_date.strftime('%B')
-    @expenses = Expense.where("category_id >= :category AND type_id <= :type" {category: params[:category_filter], type: params[:type_filter]})
+    # old_type = nil
+    # old_category = nil
+    type_id = params[:type_id] if params[:type_id] != nil
+    category_id = params[:category_id] if params[:category_id] != nil
+    # old_type = type_id if type_id != nil
+    # old_category = category_id if category_id != nil
+
     @categories = Category.all
     @types = Type.all
+
+    puts "TYPE_ID: #{type_id}"
+    puts "CATEGORY_ID: #{category_id}"
+
+    if category_id == nil && type_id == nil
+      @expenses = Expense.all
+    else
+      @expenses = Expense.where("category_id = ? AND type_id = ?", category_id, type_id) if (category_id != nil && type_id != nil)
+      @expenses = Expense.where("category_id = ?", category_id) if (category_id != nil && type_id == nil)
+      @expenses = Expense.where("type_id = ?", type_id) if (category_id == nil && type_id != nil)
+    end
+    # old_type = type_id
+    # old_category = category_id
   end
+
+  # private
+  # def params_safe
+  #
+  # end
 end
