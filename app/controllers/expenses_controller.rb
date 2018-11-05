e43eaf077e2f586ef5d701b9fda33ecd10aeeb4a
stivendiaz <stivendiazagudelo@gmail.com>
2018-11-05 01:23:57 -0500

    [ADD] date filter

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,20 +7,39 @@ class ExpensesController < ApplicationController
     @types = Type.all
     @months = []
     12.times do |i|
-      @months << DateTime.now.months_ago(i).strftime("%B %Y")
+      @months << DateTime.now.months_ago(i)
     end
-
     @current_month = @months[params[:month_filter].to_i]
-
+    month = @current_month.month
+    year = @current_month.year
+    puts "MONTH: #{@current_month.month}"
+    puts "YEAR: #{@current_month.year}"
     puts "TYPE_ID: #{type_id}"
     puts "CATEGORY_ID: #{category_id}"
 
+
     if category_id == nil && type_id == nil
-      @expenses = Expense.all
+      # @expenses = Expense.all
+      @expenses = Expense.find_year(year).find_month(month)
     else
-      @expenses = Expense.where("category_id = ? AND type_id = ?", category_id, type_id) if (category_id != nil && type_id != nil)
-      @expenses = Expense.where("category_id = ?", category_id) if (category_id != nil && type_id == nil)
-      @expenses = Expense.where("type_id = ?", type_id) if (category_id == nil && type_id != nil)
+      @expenses = Expense.find_category(category_id).find_type(type_id) if (category_id != nil && type_id != nil)
+      @expenses = Expense.find_category(category_id) if (category_id != nil && type_id == nil)
+      @expenses = Expense.find_type(type_id) if (category_id == nil && type_id != nil)
     end
   end
+
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+
+  end
+
+  protected
+
+  def safe_params
+    params.require(:expense).permit(:type_id, :date, :concept, :category_id, :amount)
+  end
+
 end
--- app/models/expense.rb
+++ app/models/expense.rb
@@ -4,4 +4,9 @@ class Expense < ApplicationRecord
 
   validates :concept, :date, :amount, presence: true
   validates :amount, numericality: {greater_than_or_equal_to: 0}
+
+  scope :find_category, ->(category) {Expense.where("category_id = ?", category)}
+  scope :find_type, ->(type) {Expense.where("type_id = ?", type)}
+  scope :find_month, ->(month) {Expense.where('extract(month from date) = ?', month)}
+  scope :find_year, ->(year) {Expense.where('extract(year from date) = ?', year)}
 end
