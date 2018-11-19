507442d42faaea420affe4544937ff4f43a681e3
stivendiaz <stivendiazagudelo@gmail.com>
2018-11-18 23:22:55 -0500

    Add expense model unit tests

--- test/controllers/expenses_controller_test.rb
+++ test/controllers/expenses_controller_test.rb
@@ -1,7 +1,5 @@
 require 'test_helper'
 
 class ExpensesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
 end
--- test/models/expense_test.rb
+++ test/models/expense_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class ExpenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "expense is not saved without a concept" do
+    expense = Expense.new(date: DateTime.now, amount: 3000, category: categories(:category_1), type: types(:type_1))
+    assert_not expense.save, "Expense is saved without a concept"
+  end
+  test "expense is not saved without a date" do
+    expense = Expense.new(concept: 'Fred es di best', amount: 3000, category: categories(:category_1), type: types(:type_1))
+    assert_not expense.save, "Expense is saved without a date"
+  end
+  test "expense is not saved without a amount" do
+    expense = Expense.new(date: DateTime.now, concept: 'Cat cat cat', category: categories(:category_1), type: types(:type_1))
+    assert_not expense.save, "Expense is saved without a amount"
+  end
+  test "expense is not saved  with a negative amount" do
+    expense = Expense.new(amount: -3000, date: DateTime.now, concept: 'Cat cat cat', category: categories(:category_1), type: types(:type_1))
+    assert_not expense.save, "Expense is saved with a negative amount"
+  end
+
 end
